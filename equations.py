932c8c2f96f0577b51ae98fdf40d96d785af8320
Hen-Shifhon <hen4775@gmail.com>
2021-03-17 22:43:28 +0200

    new

--- equations.py
+++ equations.py
@@ -10,7 +10,9 @@ def exponent (x):
         i+=1
     return result
 
-def ln(x):
+def Ln(x):
+    if(x<=0):
+        return 0.0
     y_n=x-1.0
     y_n1= y_n + 2 * (x-exponent(y_n))/(x+exponent(y_n))
     while( abss(y_n-y_n1)>0.001):
@@ -24,31 +26,20 @@ def abss(x):
     return -x
 
 def XtimesY(x,y):
-    if(x>=0):
-        return  float('%0.6f' % exponent(y*ln(x)))
+    if(x>0.0):
+        return  float('%0.6f' % exponent(y*Ln(x)))
     return 0.0
 def sqrt(x,y):
+    if (y<0 or x==0):
+        return 0.0   
     return XtimesY(y,1/x)
 
 def calculate(x):
     answer= exponent(x)*XtimesY(7,x)*XtimesY(x,-1)*sqrt(x,x)
     return answer
   
-x= input("enter a x:")
-try: 
-    x= float(x)
-    print(calculate(x)) 
-except:
-    x=0.0
-    print(x)
 
-   
-
-#print(XtimesY(x,y))
-#print(sqrt(x, y)) 
-
-
-
+                      
 
 
   
